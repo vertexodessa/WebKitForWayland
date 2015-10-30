d63223cc88a74be5ace4e9d181840856c4a898c0
Gwang Yoon Hwang <yoon@igalia.com>
2015-10-30 16:53:04 +0900

    Modified to unmap the VideoFrame asynchrously in Gst's gl thread.
    Unmapping in the compositing thread will block entire compositing loop
    to synchronize with gstreamer's gl thread.
    This patch passes unmapping job to the gstreamer's gl thread to avoid
    this overhead.

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
@@ -219,17 +219,32 @@ public:
         m_flags = GST_VIDEO_INFO_HAS_ALPHA(&videoInfo) ? TextureMapperGL::ShouldBlend : 0;
 
         GstBuffer* buffer = gst_sample_get_buffer(&sample);
-        if (UNLIKELY(!gst_video_frame_map(&m_videoFrame, &videoInfo, buffer, static_cast<GstMapFlags>(GST_MAP_READ | GST_MAP_GL))))
+        m_videoFrame = new GstVideoFrame();
+        if (UNLIKELY(!gst_video_frame_map(m_videoFrame, &videoInfo, buffer, static_cast<GstMapFlags>(GST_MAP_READ | GST_MAP_GL))))
             return;
 
-        m_textureID = *reinterpret_cast<GLuint*>(m_videoFrame.data[0]);
+        m_textureID = *reinterpret_cast<GLuint*>(m_videoFrame->data[0]);
         m_isValid = true;
     }
     virtual ~GstVideoFrameHolder()
     {
         if (UNLIKELY(!m_isValid))
             return;
-        gst_video_frame_unmap(&m_videoFrame);
+
+        GstMapInfo* info = &m_videoFrame->map[0];
+        GstGLBaseBuffer* mem = (GstGLBaseBuffer*)info->memory;
+        GstGLWindow* gstWindow = gst_gl_context_get_window(mem->context);
+
+        gst_gl_window_send_message_async(gstWindow, (GstGLWindowCB) unmapVideoFrameCallback, m_videoFrame, (GDestroyNotify) freeVideoFrameCallback);
+    }
+
+    static void unmapVideoFrameCallback(GstVideoFrame* videoFrame)
+    {
+        gst_video_frame_unmap(videoFrame);
+    }
+    static void freeVideoFrameCallback(GstVideoFrame* videoFrame)
+    {
+        delete videoFrame;
     }
 
     IntSize size() const { return m_size; }
@@ -238,7 +253,7 @@ public:
     bool isValid() const { return m_isValid; }
 
 private:
-    GstVideoFrame m_videoFrame;
+    GstVideoFrame* m_videoFrame;
     IntSize m_size;
     TextureMapperGL::Flags m_flags;
     GLuint m_textureID;
